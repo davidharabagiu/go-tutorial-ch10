404403cd253a154fc402798e51f9f4012938447c
David Harabagiu <harabagiudavid@gmail.com>
2026-06-21 22:56:08 +0300

    add doc

--- math/math.go
+++ math/math.go
@@ -1,5 +1,10 @@
+// Package math contains functions for performing to advanced mathematical operations.
 package math
 
+// Add performs [addition] on two numbers and returns the result.
+//
+// It only works on ints.
+// [addition]: https://www.mathsisfun.com/numbers/addition.html
 func Add(x, y int) int {
 	return x + y
 }
